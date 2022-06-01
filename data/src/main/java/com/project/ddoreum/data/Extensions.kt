9272dc5273bc019a9d3d41ff943eeb7946b0c841
jaeyunn <wodbs135@gmail.com>
2022-06-01 15:37:36 +0900

    [#feature/3] ApiResult 반환 타입 Extension 생성

--- data/src/main/java/com/project/ddoreum/data/Extensions.kt
+++ data/src/main/java/com/project/ddoreum/data/Extensions.kt
@@ -1,4 +1,35 @@
 package com.project.ddoreum.data
 
-class Extensions {
+import com.project.ddoreum.data.ApiErrorCase.COMMON_ERROR
+import com.project.ddoreum.data.ApiErrorCase.HTTP_EXCEPTION_ERROR
+import com.project.ddoreum.data.ApiErrorCase.NETWORK_ERROR
+import com.project.ddoreum.domain.ApiResult
+import retrofit2.HttpException
+import retrofit2.Response
+
+suspend fun <T> getResult(apiCall: suspend () -> Response<T>): ApiResult<T> {
+    try {
+        val response = apiCall()
+
+        if (response.isSuccessful) {
+            val body = response.body()
+            body?.let {
+                return ApiResult.success(body)
+            }
+        }
+
+        return ApiResult.error(COMMON_ERROR)
+
+    } catch (e: HttpException) {
+        return ApiResult.error(HTTP_EXCEPTION_ERROR)
+
+    } catch (e: Exception) {
+        return ApiResult.error(NETWORK_ERROR)
+    }
+}
+
+object ApiErrorCase {
+    const val COMMON_ERROR = "server_error"
+    const val HTTP_EXCEPTION_ERROR = "http_exception_error"
+    const val NETWORK_ERROR = "network_error"
 }
--- domain/src/main/java/com/project/ddoreum/domain/Extensions.kt
+++ domain/src/main/java/com/project/ddoreum/domain/Extensions.kt
@@ -1,4 +1,17 @@
 package com.project.ddoreum.domain
 
-class Extensions {
+data class ApiResult<out T>(val status: Status, val responseData: T?, val error: String?) {
+    enum class Status {
+        SUCCESS, ERROR
+    }
+
+    companion object {
+        fun <T> success(responseData: T): ApiResult<T> {
+            return ApiResult(Status.SUCCESS, responseData, null)
+        }
+
+        fun <T> error(error: String?): ApiResult<T> {
+            return ApiResult(Status.ERROR, null, error)
+        }
+    }
 }
