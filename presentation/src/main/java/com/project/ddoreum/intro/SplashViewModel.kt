4c723489353a0ff293d116a2f5be269434e178c4
younghoi <taleskoo@gmail.com>
2022-07-06 02:39:01 +0900

    [feature/2] Google Sign In API 연결

--- presentation/src/main/java/com/project/ddoreum/intro/SplashViewModel.kt
+++ presentation/src/main/java/com/project/ddoreum/intro/SplashViewModel.kt
@@ -3,6 +3,8 @@ package com.project.ddoreum.intro
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.tasks.Task
 import com.project.ddoreum.core.BaseViewModel
 import com.project.ddoreum.di.MainDispatcher
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -12,6 +14,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @HiltViewModel
 class SplashViewModel @Inject constructor(
@@ -38,7 +41,7 @@ class SplashViewModel @Inject constructor(
     fun setPermissionCompleted() {
         _titleState.value = true
         viewModelScope.launch(mainDispatcher) {
-            _state.emit(SplashState.Login)
+            _state.emit(SplashState.SuccessPermission)
         }
     }
 
@@ -48,11 +51,27 @@ class SplashViewModel @Inject constructor(
         }
     }
 
-    // TODO : 로그인 로직으로 이동해야 함
-    // 우선 메인액티비티로 이동
+    fun handleUserInformation(data: Task<GoogleSignInAccount>) {
+        runCatching {
+            Timber.d("data received")
+            data.result
+        }.onSuccess {
+            Timber.d("data received Success")
+            // TODO : 또오름 Login API 연동
+            viewModelScope.launch(mainDispatcher) {
+                _state.emit(SplashState.Finish(it.displayName ?: "또오름"))
+            }
+        }.onFailure {
+            Timber.d("data received Failed")
+            viewModelScope.launch(mainDispatcher) {
+                _state.emit(SplashState.FailLogin)
+            }
+        }
+    }
+
     fun onClickLogin() {
         viewModelScope.launch(mainDispatcher) {
-            _state.emit(SplashState.Finish)
+            _state.emit(SplashState.Login)
         }
     }
 }
